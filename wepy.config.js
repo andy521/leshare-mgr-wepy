e6c0e0d2ac3d50e89cb38fe913910dc46722726f
coolhwm <coolhwm@qq.com>
2017-11-28 20:50:03 +0800

    INIT

---
+++ wepy.config.js
@@ -0,0 +1,61 @@
+const prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  wpyExt: '.wpy',
+  build: {
+    web: {
+    }
+  },
+  eslint: true,
+  compilers: {
+    sass: {
+      outputStyle: 'compact'
+    },
+    babel: {
+      sourceMap: true,
+      presets: [
+        'es2015',
+        'stage-1'
+      ],
+      plugins: [
+        'transform-decorators-legacy',
+        'transform-export-extensions',
+        'syntax-export-extensions'
+      ]
+    }
+  },
+  plugins: {
+    'autoprefixer': {
+      filter: /\.(wxss|css)$/,
+      config: {
+        browsers: ['last 11 iOS versions']
+      }
+    }
+  }
+};
+
+if (prod) {
+  delete module.exports.compilers.babel.sourcesMap;
+  // 压缩sass
+  module.exports.compilers['sass'] = {outputStyle: 'compressed'};
+
+  // 压缩js
+  module.exports.plugins = {
+    uglifyjs: {
+      filter: /\.js$/,
+      config: {
+      }
+    },
+    imagemin: {
+      filter: /\.(jpg|png|jpeg)$/,
+      config: {
+        jpg: {
+          quality: 80
+        },
+        png: {
+          quality: 80
+        }
+      }
+    }
+  }
+}
