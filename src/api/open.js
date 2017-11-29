fc5a4ec8d3462ea06be21553fc05f1fb1e0b4d54
lin19950219 <779897971@qq.com>
2017-11-29 16:46:27 +0800

    ADD:完成草稿列表

--- src/api/open.js
+++ src/api/open.js
@@ -5,9 +5,13 @@ export default class open extends base {
   /**
    * 查询所有草稿
    */
-  static drafts() {
+  static async drafts() {
     const url = `${open.baseUrl}/code/draft_list`;
-    return this.get(url);
+    const drafts = await this.get(url);
+    drafts.forEach(draft => {
+      draft.create_time = this.convertToTime(draft.create_time);
+    });
+    return drafts;
   }
   /**
    * 查询所有模板
@@ -15,5 +19,11 @@ export default class open extends base {
   static templates() {
     const url = `${open.baseUrl}/code/template_list`;
     return this.get(url);
+  }
+
+  // 处理时间
+  static convertToTime (time) {
+    const d = new Date(time * 1000);
+    return `${d.getFullYear()}/${d.getMonth()}/${d.getDay()} ${d.getHours()}:${d.getMinutes()}`;
   }
 }
