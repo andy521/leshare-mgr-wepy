96907b203f59708d3f9710cc9fc209c8adfa5314
coolhwm <coolhwm@qq.com>
2017-11-29 10:37:10 +0800

    INIT:初始化基础页面

--- src/api/open.js
+++ src/api/open.js
@@ -2,4 +2,18 @@ import base from './base'
 import wepy from 'wepy';
 
 export default class open extends base {
+  /**
+   * 查询所有草稿
+   */
+  static drafts() {
+    const url = `${open.baseUrl}/code/draft_list`;
+    return this.get(url);
+  }
+  /**
+   * 查询所有模板
+   */
+  static templates() {
+    const url = `${open.baseUrl}/code/template_list`;
+    return this.get(url);
+  }
 }
